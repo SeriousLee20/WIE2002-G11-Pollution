188386fab49f0bee81a0a4faed739a8107c99b5d
ooixiegee <75740676+ooixiegee@users.noreply.github.com>
2022-04-24 04:50:41 +0800

    land

--- assets/js/main.js
+++ assets/js/main.js
@@ -263,3 +263,64 @@
   });
 
 })()
+
+
+/*=============== HOME SWIPER ===============*/
+let homeSwiper = new Swiper(".home-swiper", {
+  spaceBetween: 30,
+  loop: 'true',
+  
+  pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+})
+
+
+/*=============== NEW SWIPER ===============*/
+let newSwiper = new Swiper(".new-swiper", {
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: 'true',
+  spaceBetween: 16,
+});
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current =>{
+      const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id')
+
+      if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+          document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+      }else{
+          document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+      }
+  })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 460 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if(this.scrollY >= 460) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  // reset: true
+})
+
+sr.reveal(`.home-swiper, .new-swiper, .newsletter_container`)
+
